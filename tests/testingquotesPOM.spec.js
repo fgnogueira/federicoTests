2c541237a2f580163b066aa227b68422d7368ae2
fgnogueira <federicogonzaleznogueira@hotmail.com>
2024-07-04 15:19:33 +0200

    removing the explicit waits

--- tests/testingquotesPOM.spec.js
+++ tests/testingquotesPOM.spec.js
@@ -7,6 +7,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('test 1 - get a quote', async ({ page }) => {
+	//await page.setViewportSize({ width: 1600, height: 1200 });
 	const login = new loginPage(page);
 	const quotes = new quotesPage(page);
 	await login.loginWithCredentials('qaautostaging@email.ghostinspector.com', 'Automation123!');
@@ -15,11 +16,9 @@ test('test 1 - get a quote', async ({ page }) => {
 	await quotes.exportButton.click();
 	await quotes.fclButton.click();
 	await quotes.fromInputField.fill('Pip');
-	await page.waitForTimeout(1000);
 	await page.keyboard.press('a');
 	await quotes.suggestionFromItem.click();
 	await quotes.toInputField.fill('New');
-	await page.waitForTimeout(1000);
 	await page.keyboard.press('Space');
 	await quotes.suggestionToItem.click();
 	await quotes.equipmentTypeBox.click();
