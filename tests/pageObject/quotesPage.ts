9eb334439f2aa251fad4ed8e3e06b66c6f7809db
fgnogueira <federicogonzaleznogueira@hotmail.com>
2024-07-05 10:12:48 +0200

    some updates to the tests

--- tests/pageObject/quotesPage.ts
+++ tests/pageObject/quotesPage.ts
@@ -1,37 +1,94 @@
-import {Locator , Page} from "@playwright/test"
+import {Keyboard, Locator , Page } from "@playwright/test"
 
 export class quotesPage {
 
-private  newBookingLink: Locator
-private  exportButton: Locator
-private  importButton: Locator
-private  fclButton: Locator
-private  lclButton: Locator
-private  fromInputField: Locator
-private  toInputField: Locator
-private  suggestionFromItem: Locator
-private  suggestionToItem: Locator
-private  equipmentTypeBox: Locator
+private  newBooking: Locator
+private  export: Locator
+private  import: Locator
+private  fcl: Locator
+private  lcl: Locator
+private  fromInput: Locator
+private  toInput: Locator
+private  suggestionFrom: Locator
+private  suggestionTo: Locator
+private  equipmentType: Locator
 private  optionIndex1: Locator
-private  viewQuotesButton: Locator
+private  viewQuotes: Locator
 private  quotesTitle: Locator
 
 
-constructor(page:Page){
 
-    this.newBookingLink = page.locator('xpath=//*[@data-test-link="new-booking"]')
-    this.exportButton = page.locator('xpath=//*[@data-test-action="export"]')
-    this.importButton = page.locator('xpath=//*[@data-test-action="import"]')
-    this.fclButton = page.locator('xpath=//*[@data-test-action="fcl"]')
-    this.lclButton = page.locator('xpath=//*[@data-test-action="lcl"]')
-    this.fromInputField = page.locator('xpath=//*[@data-test-input="from"]')
-    this.toInputField = page.locator('xpath=//*[@data-test-input="to"]')
-    this.suggestionFromItem = page.locator('xpath=//*[@data-test-data="suggestion-from"]')
-    this.suggestionToItem = page.locator('xpath=//*[@data-test-data="suggestion-to"]')
-    this.equipmentTypeBox = page.locator('xpath=//*[@data-test-input="0-equipment-type"]')
+constructor(page:Page, keyboard:Keyboard){
+
+    this.newBooking = page.locator('xpath=//*[@data-test-link="new-booking"]')
+    this.export = page.locator('xpath=//*[@data-test-action="export"]')
+    this.import = page.locator('xpath=//*[@data-test-action="import"]')
+    this.fcl = page.locator('xpath=//*[@data-test-action="fcl"]')
+    this.lcl = page.locator('xpath=//*[@data-test-action="lcl"]')
+    this.fromInput = page.locator('xpath=//*[@data-test-input="from"]')
+    this.toInput = page.locator('xpath=//*[@data-test-input="to"]')
+    this.suggestionFrom = page.locator('xpath=//*[@data-test-data="suggestion-from"]')
+    this.suggestionTo = page.locator('xpath=//*[@data-test-data="suggestion-to"]')
+    this.equipmentType = page.locator('xpath=//*[@data-test-input="0-equipment-type"]')
     this.optionIndex1 = page.locator('xpath=//*[@data-option-index="0"]')
-    this.viewQuotesButton = page.locator('xpath=//*[@data-test-action="view-quotes"]')
+    this.viewQuotes = page.locator('xpath=//*[@data-test-action="view-quotes"]')
     this.quotesTitle = page.locator('xpath=//*[@class="quote-tools-holder"]/h1')
+   
+
+}
+
+async newBookingLink(){
+    await this.newBooking.click();
+}
+
+
+async exportButton(){
+    await this.export.click();
+}
+
+
+async importButton(){
+    await this.import.click();
+}
+
+
+async fclButton(){
+    await this.fcl.click();
+}
+
+
+async lclButton(){
+    await this.lcl.click();
+}
+
+
+async fromInputField(from:string){
+    await this.fromInput.fill(from);
+}
+
+
+async toInputField(to:string){
+    await this.toInput.fill(to);
+}
+
+
+async suggestionFromItem(){
+    await this.suggestionFrom.click();
+}
+
+
+async suggestionToItem(){
+    await this.suggestionTo.click();
+}
+
+
+async equipmentTypeBox(){
+    await this.equipmentType.click();
+}
+
+
+async viewQuotesButton(){
+    await this.viewQuotes.click();
 }
 
 }
